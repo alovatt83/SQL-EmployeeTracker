bbdc9d1b8da4c420a63d3f87a3174c8a60a41e93
Allen <allenlovatt@gmail.com>
2021-02-26 14:53:08 -0500

    Added View all Emplyees function

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { query } = require("express");
 const inquirer = require("inquirer");
 const mysql = require('mysql');
 require('console.table');
@@ -44,37 +45,35 @@ function prompt(){inquirer.prompt({
         menuList.addRole,
         menuList.addEmployee,
         menuList.updateEmployee,
-        menuList.quit
-            ]
+        menuList.quit]
     })
 
-    .then(response => {
-            switch (response.nextAction) {
-                case menuList.viewDepartments:
-                    viewDepartments();
-                    break;
-                case menuList.addRole:
-                    viewRoles();
-                    break;
-                case menuList.viewEmployees:
-                    viewEmployees();
-                    break;
-                case menuList.addDep:
-                    addDep();
-                    break;
-                case menuList.addRole:
-                    addRole();
-                    break;
-                case menuList.addEmployee:
-                    addEmployee();
-                    break;
-                case menuList.updateEmployee:
-                    updateEmployee();
-                    break;
-                case 'Quit':
-                    console.log('Exiting System')
-                    connection.end();
-                    break;
+.then(response => {
+    switch (response.nextAction) {
+        case menuList.viewDepartments:
+            viewDepartments();
+            break;
+        case menuList.addRole:
+            viewRoles();
+            break;
+        case menuList.viewEmployees:
+            viewEmployees();
+            break;
+        case menuList.addDep:
+            addDep();
+            break;
+        case menuList.addRole:
+            addRole();
+            break;
+        case menuList.addEmployee:
+            addEmployee();
+            break;
+        case menuList.updateEmployee:
+            updateEmployee();
+            break;
+        case 'Quit':
+            connection.end();
+            break;
             }
     
     ]);
@@ -105,4 +104,17 @@ function viewRoles() {
         console.log(View all roles');
         prompt();
     });
+}
+
+function viewEmployees() {
+    const query = 'SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.firstname, ' ', manager.last_name) AS manager
+    FROM employee
+    LEFT JOIN employee manager ON manager.id = employee.manager_id
+    INNER JOIN role ON (role.id = employee.role_id)
+    INNER JOIN department ON (department.id = role.department_id)
+    ORDER BY employee.id;';
+    console.log('View employees: ');
+    prompt();
+});
+
 }
